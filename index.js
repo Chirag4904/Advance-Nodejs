e63d8a171762dd459a925c0650728e00ef0992bd
chiggy69 <chiragaggarwal69@gmail.com>
2023-06-13 14:24:14 +0530

    forking children processes

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function someWork(duration) {
-	const start = Date.now();
-	while (Date.now() - start < duration) {
-		//do nothing
+// is this file being executed in master mode or not
+
+if (cluster.isMaster) {
+	//if it is in master mode then execute index.js again in child/slave mode
+	cluster.fork();
+
+	// cluster.fork();
+} else {
+	// This is child code and this will be the server
+	const express = require("express");
+	const app = express();
+
+	function someWork(duration) {
+		const start = Date.now();
+		while (Date.now() - start < duration) {
+			//do nothing
+		}
 	}
+
+	app.get("/", (req, res) => {
+		someWork(5000); // this will block the event loop for 5 seconds and no other req will be served in this time.
+		res.send("Hello mate");
+	});
+
+	app.get("/fast", (req, res) => {
+		res.send("This was fast");
+	});
+
+	app.listen(3000, () => {
+		console.log("Listening on port 3000");
+	});
 }
-
-app.get("/", (req, res) => {
-	someWork(5000); // this will block the event loop for 5 seconds and no other req will be served in this time.
-	res.send("Hello mate");
-});
-
-app.listen(3000, () => {
-	console.log("Listening on port 3000");
-});
